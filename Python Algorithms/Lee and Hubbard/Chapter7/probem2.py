48a75afc367f9745db250009d03199b1ca3d131b
Stephen O'Sullivan <osullisg@gmail.com>
2015-08-18 21:09:17 -0700

    Chapter 7 from Hubbard: Graphs

--- Python Algorithms/Lee and Hubbard/Chapter7/probem2.py
+++ Python Algorithms/Lee and Hubbard/Chapter7/probem2.py
@@ -1,8 +1,11 @@
 __author__ = 'stephenosullivan'
 
 import xml.etree.ElementTree as ET
+from decimal import getcontext, Decimal
 tree = ET.parse('graph.xml')
 root = tree.getroot()
+
+getcontext().prec = 3
 
 class Vertex:
     def __init__(self, label):
@@ -44,21 +47,39 @@ class Graph:
             self.addvertex(v1)
         if v2 not in self.vertices:
             self.addvertex(v2)
-        self.vertices[v1].adjacent.add(v2)
+        self.vertices[v1].adjacent.add((v2, w))
 
     def bfs(self, start, end):
         queue = [(start, 1)]
         visited = set()
         while queue:
             current, count = queue.pop(0)
-            for neighbor in current.adjacent:
+            for neighbor, w in current.adjacent:
                 if neighbor == end.label:
                     return count
                 if neighbor not in visited:
                     queue.append((self.vertices[neighbor], count + 1))
+                visited.add(neighbor)
         return None
 
     def dijkstra(self, start):
+        queue = [(start, Decimal(0))]
+        self.vertices[start].previous = Decimal(0)
+        while queue:
+            print(queue)
+            current, weight = queue.pop(0)
+            #print(type(self.vertices[current]))
+            #print("test", current)
+            for (neighbor, edge_weight) in self.vertices[current].adjacent:
+                #print(neighbor.label)
+                if self.vertices[neighbor].previous is None:
+                    self.vertices[neighbor].previous = weight + Decimal(edge_weight)
+                    queue.append((neighbor, self.vertices[neighbor].previous))
+                elif self.vertices[neighbor].previous > weight + Decimal(edge_weight):
+                    self.vertices[neighbor].previous = weight + Decimal(edge_weight)
+                    queue.append((neighbor, self.vertices[neighbor].previous))
+
+        return [(label, vertex.previous) for (label, vertex) in self.vertices.items()]
 
     #def kruskal(self):
         # sort edges
@@ -68,7 +89,7 @@ class Graph:
 
 
 if __name__ == '__main__':
-
+    directed = False
     """Load data from XML file"""
     Vertices = set()
     Edges = set()
@@ -80,6 +101,8 @@ if __name__ == '__main__':
                 V_map[grandchild.attrib['vertexId']] = grandchild.attrib['label']
             if grandchild.tag == 'Edge':
                 Edges.add((int(V_map[grandchild.attrib['tail']]), int(V_map[grandchild.attrib['head']]), float(grandchild.attrib['weight'])))
+                if directed is False:
+                    Edges.add((int(V_map[grandchild.attrib['head']]), int(V_map[grandchild.attrib['tail']]), float(grandchild.attrib['weight'])))
 
     graph = Graph()
     for vertex in Vertices:
@@ -89,12 +112,18 @@ if __name__ == '__main__':
         graph.addedge(edge)
 
     """Need to make some adjustments to match graph in Fig.7.9"""
-    graph.vertices[2].adjacent = {8}
-    graph.vertices[8].adjacent = {13}
-    graph.vertices[13].adjacent = {14, 16}
-    graph.vertices[17].adjacent = {3, 6, 23, 24}
-    graph.vertices[18].adjacent = {17}
-    graph.vertices[19].adjacent = {18}
-    graph.vertices[23].adjacent = {24}
+    if directed:
+        graph.vertices[2].adjacent = {(8, 4.64)}
+        graph.vertices[8].adjacent = {(13, 8.45)}
+        graph.vertices[13].adjacent = {(14, 4.61), (16, 7.32)}
+        graph.vertices[17].adjacent = {(3, 10.81), (6, 11.6), (23, 11.76), (24, 8.42)}
+        graph.vertices[18].adjacent = {(17, 4.07)}
+        graph.vertices[19].adjacent = {(18, 1.54)}
+        graph.vertices[23].adjacent = {(24, 2.18)}
+
+
 
     print(graph.bfs(graph.vertices[9], graph.vertices[16]))
+    print(graph.dijkstra(0))
+    #print(graph.vertices[29].adjacent)
+    #print(graph.vertices[28].adjacent)
